c8aa44eec08f4da1c1b379c78ef61cd01577b709
grantregen <gregen@stanford.edu>
2022-05-12 14:51:00 -0700

    finishing up GVE equations

---
+++ Qlaw.jl
@@ -0,0 +1,87 @@
+using StaticArrays
+
+# constants
+µ = 3.986004419 * 10^14
+
+struct EquinoctialOrbit
+    p # semi latus rectum [meters]
+    f
+    g
+    h
+    k
+    L # true longitude [rad]
+end
+
+struct KeplarianOrbit
+    a # semi-major axis [meters]
+    e # eccentricity [-]
+    i # inclinations [rad]
+    ω # arg. of perigee [rad]
+    Ω # raan [rad]
+    ν # true anomaly [rad]
+end
+
+function Equinoctial2Keplarian(orbit::KeplarianOrbit)
+    # unload orbit parameters
+    p = orbit.p
+    f = orbit.f
+    g = orbit.g
+    h = orbit.h
+    k = orbit.k
+    L = orbit.L
+    # convert orbital elements
+    # from https://spsweb.fltops.jpl.nasa.gov/portaldataops/mpg/MPG_Docs/Source%20Docs/EquinoctalElements-modified.pdf
+    a = p/(1-f^2-g^2)
+    e = sqrt(f^2 + g^2)
+    i = atan(2*sqrt(h^2+k^2), 1-h^2-k^2)
+    ω = atan(g*h-f*k, f*h+g*k)
+    Ω = atan(k,h)
+    ν = L - atan(g,f)
+    return(KeplarianOrbit(a, e, i, ω, Ω, ν))
+end
+
+function Keplarian2Equinoctial(orbit::KeplarianOrbit)
+    # unload orbit parameters
+    a = orbit.a
+    e = orbit.e
+    i = orbit.i
+    ω = orbit.ω
+    Ω = orbit.Ω
+    ν = orbit.ν
+    # convert orbital elements
+    p = a*(1-e^2)
+    f = e*cos(ω+Ω)
+    g = e*sin(ω+Ω)
+    h = tan(i/2)*cos(Ω)
+    k = tan(i/2)*sin(Ω)
+    L = Ω + ω + ν
+    return(EquinoctialOrbit(p,f,g,h,k,L))
+end
+
+function GaussVariationalEquationsEquinoctial(orbit, accel)
+    # unpack orbit
+    p = orbit[1] # semi latus rectum [meters]
+    f = orbit[2]
+    g = orbit[3]
+    h = orbit[4]
+    k = orbit[5]
+    L = orbit[6] # true longitude [rad]
+
+    # calcualte useful quantities
+    cosL = cos(L)
+    sinL = sin(L)
+    w = 1 + f*cosL+g*sinL
+    root_p_µ = sqrt(p/µ)
+    s_2 = 1 + h^2 + k^2
+
+    # calculate matrix
+    A = [[0, sinL, -cosL, 0, 0, 0]
+         [2*p/w, (1/w)*((w+1)*cosL+f), (w+1)*sinL+g, 0, 0, 0]
+         [0, -(g/w)*(h*sinL-k*cosL), (f/w)*(h*sinL-k*cosL), (s_2*cosL)/(2*w), (s_2*sinL)/(2*w), h*sinL-k*cosL]]
+    A = []
+    A = root_p_µ * A
+    b = [0, 0, 0, 0, 0, sqrt(µ*p)*(w/p)^2]
+
+    # calculate doe/dt
+    dOEdt = A
+end
