4c0a3797e473b0e088eb3d966ef7638d502f57cc
Flynn Dreilinger <flynnd@stanford.edu>
2022-05-27 17:29:38 -0700

    add dwell script

--- JQlaw.jl
+++ JQlaw.jl
@@ -2,5 +2,19 @@ __precompile__()
 module JQlaw
     include("Qlaw.jl")
     include("Integrator.jl")
-    include("Dwell.jl")
+    
+    # Qlaw
+    export µ
+    export R_e
+    export EquinoctialOrbit
+    export KeplarianOrbit
+    export Equinoctial2Keplarian
+    export Equinoctial2ECI
+    export Keplarian2Equinoctial
+    export GaussVariationalEquationsEquinoctial
+    export QParams
+    export calc_D
+
+    # integrator
+    export RK45
 end
